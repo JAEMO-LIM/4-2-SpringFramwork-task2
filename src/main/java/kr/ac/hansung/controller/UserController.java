e65d23dc63e3f5ea33ba292aa9d5b01185295600
JAEMO-LIM <tjdwkddl@gmail.com>
2026-06-05 00:54:58 +0900

    feat: UserController @Controller 선언 및 비밀번호 변경 GET 엔드포인트 추가

--- src/main/java/kr/ac/hansung/controller/UserController.java
+++ src/main/java/kr/ac/hansung/controller/UserController.java
@@ -3,17 +3,30 @@ package kr.ac.hansung.controller;
 import jakarta.validation.Valid;
 import kr.ac.hansung.dto.PasswordChangeDto;
 import kr.ac.hansung.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+
+@Controller
+@RequiredArgsConstructor
 public class UserController {
 
-    private UserService userService;
-    
+    private final UserService userService;
+
+    @GetMapping("/user/password")
+    public String passwordForm(Model model) {
+        model.addAttribute("passwordChangeDto", new PasswordChangeDto());
+        return "user/password";
+    }
+
     @PostMapping("/user/password")
     public String changePassword(
             @AuthenticationPrincipal UserDetails userDetails,
